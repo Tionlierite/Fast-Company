57a0089cd38192aa39a59ec47b32f2c723bbdce3
Tionlierite <Tionlierite@gmail.com>
2023-04-19 00:37:49 +0300

    feat: main branch became Template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import React from 'react'
 import reactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-
-import Counter from "./Components/counter";
-
-const App = () => {
-	return <Counter/>
-}
-
-reactDOM.render(<App />, document.querySelector('#root'))
