6b4a0827d8fa3dc2afcf795eaf02b024b25ea9cf
Tionlierite <Tionlierite@gmail.com>
2023-04-19 00:14:35 +0300

    feat: state of rep after lecture 11

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -25,19 +25,25 @@ const Counter = () => {
 		setTags(prevState => prevState.filter(tag => tag !== id))
 	}
 
+	const renderTags = () => {
+		if (tags.length === 0) return 'no tags'
+		return tags.map(tag => (
+			<li
+				key={tag}
+				className='btn btn-primary btn-sm m-2'
+				onClick={() => handleTagChange(tag)}
+			>
+				{tag}
+			</li>
+		))
+	}
+
+	if (tags.length !== 0) {
+		return <ul>{renderTags()}</ul>
+	}
+
 	return (
 		<>
-			<ul>
-				{tags.map(tag => (
-					<li
-						key={tag}
-						className='btn btn-primary btn-sm m-2'
-						onClick={() => handleTagChange(tag)}
-					>
-						{tag}
-					</li>
-				))}
-			</ul>
 			<span className={getBadgeClasses()}>{formatCount()}</span>
 			<button
 				className='btn bg-primary btn-sm m-2'
