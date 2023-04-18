7f8dabf87f4bcfb007c5334a240c3ecd8f3f9c1b
Tionlierite <Tionlierite@gmail.com>
2023-04-19 00:01:51 +0300

    feat: state of rep after lecture 10

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -21,8 +21,8 @@ const Counter = () => {
 		setCount(prevState => prevState - 1)
 	}
 
-	const handleTagChange = () => {
-		setTags(['tag4', 'tag5'])
+	const handleTagChange = id => {
+		setTags(prevState => prevState.filter(tag => tag !== id))
 	}
 
 	return (
@@ -32,7 +32,7 @@ const Counter = () => {
 					<li
 						key={tag}
 						className='btn btn-primary btn-sm m-2'
-						onClick={handleTagChange}
+						onClick={() => handleTagChange(tag)}
 					>
 						{tag}
 					</li>
