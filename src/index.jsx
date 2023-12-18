9efff48cdc2565654e45b44876497c66ff8e8122
Tionlierite <Tionlierite@gmail.com>
2023-12-18 16:22:16 +0300

    [feat] Migrated to Vite, refactor code

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "bootstrap/dist/css/bootstrap.css"
+
+import Users from "./app/components/users"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+	<React.StrictMode>
+		<Users />
+	</React.StrictMode>
+)
