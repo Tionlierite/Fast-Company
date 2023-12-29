61d046295bb339005cf548f84a836f89963363ba
Tionlierite <Tionlierite@gmail.com>
2023-12-29 15:14:39 +0300

    [impr] Added prettier-ignore to the block of code in UsersTable

--- src/app/components/UsersTable.jsx
+++ src/app/components/UsersTable.jsx
@@ -13,48 +13,14 @@ export const UsersTable = ({ users, onSort, currentSort, ...rest }) => {
 	return (
 		<table className='table'>
 			<thead>
+				{/*prettier-ignore*/}
 				<tr>
-					<th
-						onClick={() => {
-							handleSort("name")
-						}}
-						scope='col'
-					>
-						Имя
-					</th>
+					<th onClick={() => {handleSort("name")}} scope='col'>Имя</th>
 					<th scope='col'>Качества</th>
-					<th
-						onClick={() => {
-							handleSort("professions.name")
-						}}
-						scope='col'
-					>
-						Профессия
-					</th>
-					<th
-						onClick={() => {
-							handleSort("completedMeetings")
-						}}
-						scope='col'
-					>
-						Встретился, раз
-					</th>
-					<th
-						onClick={() => {
-							handleSort("rate")
-						}}
-						scope='col'
-					>
-						Оценка
-					</th>
-					<th
-						onClick={() => {
-							handleSort("bookmark")
-						}}
-						scope='col'
-					>
-						Избранное
-					</th>
+					<th onClick={() => {handleSort("professions.name")}} scope='col'>Профессия</th>
+					<th onClick={() => {handleSort("completedMeetings")}} scope='col'>Встретился, раз</th>
+					<th onClick={() => {handleSort("rate")}} scope='col'>Оценка</th>
+					<th onClick={() => {handleSort("bookmark")}} scope='col'>Избранное</th>
 					<th />
 				</tr>
 			</thead>
