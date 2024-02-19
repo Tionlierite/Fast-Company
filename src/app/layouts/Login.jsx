9032e4783cfd7cc0db8222c8f0dc4b580f21da73
Tionlierite <Tionlierite@gmail.com>
2024-02-19 16:44:09 +0300

    [feat] Added 2 forms in Login Page
    - Email and Passwords, making them Controllable components

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 
 export const Login = () => {
-	return <h1>Login</h1>
+	const [data, setData] = useState({ email: "", password: "" })
+
+	const handleChange = ({ target }) => {
+		setData(prevState => ({ ...prevState, [target.name]: target.value }))
+	}
+
+	return (
+		<form action=''>
+			<div>
+				<label htmlFor='email'>Email</label>
+				<input type='text' id='email' value={data.email} onChange={handleChange} name='email' />
+			</div>
+			<div>
+				<label htmlFor='password'>Password</label>
+				<input type='password' id='password' value={data.password} onChange={handleChange} name='password' />
+			</div>
+		</form>
+	)
 }
